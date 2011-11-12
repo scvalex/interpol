f5091d699b713422cf3ca1d3827d7894e69d3fc4
Francesco <f@mazzo.li>
2011-11-12 15:15:02 +0000

    Type classes, no typeable

--- Text/Interpol.hs
+++ Text/Interpol.hs
@@ -1,22 +1,28 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances, OverlappingInstances #-}
 -- | Support module for the @interpol@ preprocessor.
 module Text.Interpol (
         (^-^)
     ) where
 
-import Data.Typeable ( Typeable, cast )
-
 infixl 3 ^-^
 
--- | Append a showable value to a 'String' in a smart way.  In
--- particular, do /not/ 'show' a 'String', as this encloses it in
--- \"quotes\".  So, depending on the type of the second parameter,
--- '^-^' is equivalent to one of the following
---
--- @
---   x ^-^ y = x ++ y
---   x ^-^ y = x ++ show y
--- @
-(^-^) :: (Typeable a, Show a) => String -> a -> String
-s ^-^ st = case cast st of
-             Just s' -> s ++ s'
-             Nothing -> s ++ show st
+(^-^) :: Interpol a => String -> a -> String
+(^-^) = interpol
+
+class Interpol a where
+  -- | Append a showable value to a 'String' in a smart way.  In
+  -- particular, do /not/ 'show' a 'String', as this encloses it in
+  -- \"quotes\".  So, depending on the type of the second parameter,
+  -- '^-^' is equivalent to one of the following
+  --
+  -- @
+  --   x ^-^ y = x ++ y
+  --   x ^-^ y = x ++ show y
+  -- @
+  interpol :: String -> a -> String
+
+instance Interpol [Char] where
+  interpol s x = s ++ x
+
+instance Show a => Interpol a where
+  interpol s x = s ++ show x
